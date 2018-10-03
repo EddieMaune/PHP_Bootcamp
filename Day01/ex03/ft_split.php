968813d5d3eb5c6e92fe5db8c10d15c3bf3f2992
Lehlohonolo LEBAKENG <llebaken@e4r18p3.wethinkcode.co.za>
2018-10-03 16:21:29 +0200

    final

--- Day01/ex03/ft_split.php
+++ Day01/ex03/ft_split.php
@@ -3,10 +3,7 @@
 function ft_split($str)
 {
     $new_str = array_filter(explode(" ", $str));
-    // $new_str = array_filter(preg_replace('/[\t|\s{2,}]/', ' ', $str));
     sort($new_str);
-    // trim($new_str);
-    
     return ($new_str);
 }
 ?>
