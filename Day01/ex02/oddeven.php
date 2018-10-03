797056e0dc7bacfce8dc7bc244ba209c23bc6b83
Nohamba NQISHA <nnqisha@e4r18p2.wethinkcode.co.za>
2018-10-03 17:10:23 +0200

    eddy

--- Day01/ex02/oddeven.php
+++ Day01/ex02/oddeven.php
@@ -1,17 +1,22 @@
 #!/usr/bin/php
 <?PHP
+while (1)
+{
 $number;
 $oddeven;
 echo "Enter a number: ";
 $number = trim(fgets(STDIN));
+if(feof(STDIN))
+        exit("^D"."\n");
 if (is_numeric($number))
 {
 	if ($number % 2 == 0)
 		$oddeven = "even";
 	else
 		$oddeven = "odd";
-	echo "The number ".$number." is ".$oddeven;
+	echo "The number ".$number." is ".$oddeven."\n";
 }
 else
-	echo "'".$number."'"." is not a number";
+    echo "'".$number."'"." is not a number"."\n";
+}
 ?>
